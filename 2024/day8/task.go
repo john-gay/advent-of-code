cadc0c37db29f4000a88db57118e4a9fd3006536
john-gay <john.c.gay9@gmail.com>
2024-12-10 13:29:28 +0000

    complete 2024 day 8 part 2

--- 2024/day8/task.go
+++ 2024/day8/task.go
@@ -15,6 +15,8 @@ type coord struct {
 	x, y int
 }
 
+var maxX, maxY int
+
 func main() {
 	start := time.Now()
 
@@ -37,7 +39,7 @@ func run() (int, int) {
 	part1, part2 := 0, 0
 
 	grid := map[string][]coord{}
-	maxX, maxY := 0, 0
+	maxX, maxY = 0, 0
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
@@ -54,99 +56,75 @@ func run() (int, int) {
 		}
 	}
 
+	part1 = calculateAntennas(grid, false)
+	part2 = calculateAntennas(grid, true)
+
+	return part1, part2
+}
+
+func calculateAntennas(grid map[string][]coord, resonance bool) int {
 	antennaMap := map[coord]bool{}
 	for _, v := range grid {
-		for i := 0; i < len(v) - 1; i++ {
-			for j := i+1; j < len(v); j++ {
-				antenna, _ := getNewCoords(v[i], v[j], maxX, maxY)
+		for i := 0; i < len(v)-1; i++ {
+			for j := i + 1; j < len(v); j++ {
+				if resonance {
+					antennaMap[v[i]] = true
+					antennaMap[v[j]] = true
+				}
+				antenna := getNewCoords(v[i], v[j], resonance)
 				for _, a := range antenna {
 					antennaMap[a] = true
 				}
 			}
 		}
 	}
-
-	part1 = len(antennaMap)
-
-	antennaMapWithRes := map[coord]bool{}
-	for _, v := range grid {
-		for i := 0; i < len(v) - 1; i++ {
-			for j := i+1; j < len(v); j++ {
-				count := 0
-				a1, a2 := v[i], v[j]
-
-				resLoop:
-				for {
-					antennas, diffs := getNewCoords(a1, a2, maxX, maxY)
-					count++
-
-					newAntennas := []coord{}
-					fmt.Println(antennas)
-					for i, a := range antennas {
-						antennaMapWithRes[a] = true
-
-						newAntennas = append(newAntennas, coord{x: a.x + diffs[i][0], y: a.y + diffs[i][1]})
-						
-					}
-					fmt.Println("newAntennas", newAntennas)
-					antennas = newAntennas
-					if len(antennas) == 0 || count == 3 {
-						break resLoop
-					}
-				}
-			}
-		}
-	}
-
-	part2 = len(antennaMapWithRes)
-
-	return part1, part2
+	return len(antennaMap)
 }
 
-// Returns all new coords
-func getNewCoords(a coord, b coord, maxX, maxY int) ([]coord, [][]int) {
+func getNewCoords(a coord, b coord, resonance bool) ([]coord) {
 	coords := []coord{}
-	diffs := [][]int{}
 
 	diffX := getAbsDiff(a.x, b.x)
 	diffY := getAbsDiff(a.y, b.y)
 	
-	x1, x2, y1, y2 := 0, 0, 0, 0
-	dx1, dx2, dy1, dy2 := 0, 0, 0, 0
-	if a.x < b.x {
-		x1 = a.x - diffX
-		x2 = b.x + diffX
-		dx1 = -diffX
-		dx2 = diffX
-	} else {
-		x1 = a.x + diffX
-		x2 = b.x - diffX
-		dx1 = diffX
-		dx2 = -diffX
-	}
-	if a.y < b.y {
-		y1 = a.y - diffY
-		y2 = b.y + diffY
-		dy1 = -diffY
-		dy2 = diffY
-	} else {
-		y1 = a.y + diffY
-		y2 = b.y - diffY
-		dy1 = diffY
-		dy2 = -diffY
+	i := 0
+	for {
+		i++
+		added := false
+		x1, x2, y1, y2 := 0, 0, 0, 0
+		if a.x < b.x {
+			x1 = a.x - diffX * i
+			x2 = b.x + diffX * i
+		} else {
+			x1 = a.x + diffX * i
+			x2 = b.x - diffX * i
+		}
+		if a.y < b.y {
+			y1 = a.y - diffY * i
+			y2 = b.y + diffY * i
+		} else {
+			y1 = a.y + diffY * i
+			y2 = b.y - diffY * i
+		}
+
+		if x1 >= 0 && x1 < maxX && y1 >= 0 && y1 < maxY {
+			coords = append(coords, coord{x: x1, y: y1})
+			added = true
+		}
+		if x2 >= 0 && x2 < maxX && y2 >= 0 && y2 < maxY {
+			coords = append(coords, coord{x: x2, y: y2})
+			added = true
+		}
+		if resonance {
+			if !added {
+				break
+			}
+		} else {
+			break
+		}
 	}
 
-	if x1 >= 0 && x1 < maxX && y1 >= 0 && y2 < maxY {
-		coords = append(coords, coord{x: x1, y: y1})
-		diffs = append(diffs, []int{dx1, dy1})
-	}
-	
-	if x2 >= 0 && x2 < maxX && y2 >= 0 && y2 < maxY {
-		coords = append(coords, coord{x: x2, y: y2})
-		diffs = append(diffs, []int{dx2, dy2})
-	}
-
-	return coords, diffs
+	return coords
 }
 
 func getAbsDiff(a int, b int) int {
